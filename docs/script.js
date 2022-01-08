e1df3fb4c6ec7d5b774a6a229ffb0c1217788bdf
Ankur Seth <iankurseth@gmail.com>
2022-01-08 13:46:03 +0530

    Now copying to clipboard

--- docs/script.js
+++ docs/script.js
@@ -8,10 +8,19 @@ function formatJSON (e) {
 	try {
 		const jsonText = JSON.parse(inputTextArea.value);
 		outputTextArea.value = JSON.stringify(jsonText, null, 4);
+		outputTextArea.select();
 		console.log("Success.");
 	} catch (e) {
 		outputTextArea.value = "/* Error while parsing JSON! */"
 		console.log("Error while parsing JSON!");
+	}
+}
+
+function copyToClipboard (e) {
+	const outputTextArea = document.querySelector("textarea[name='outputJSON']");
+	let text = outputTextArea.value.trim();
+	if (text) {
+		navigator.clipboard.writeText(outputTextArea.value);
 	}
 }
 
